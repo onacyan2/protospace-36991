30ea8f226b0ed6d9a14f5024b7acd4a360a5c350
onacyan2 <omiz733NL@icloud.com>
2021-10-15 18:30:30 +0900

    Basic認証を導入変更

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,11 +1,8 @@
 class PrototypesController < ApplicationController
-  before_action :move_to_index, only: [:edit]
+  before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
-  
-  before_action :set_prototype, only: [:edit, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
- 
- 
   def index
     @prototypes = Prototype.all
   end
@@ -32,8 +29,8 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    if prototype.update(prototype_params)
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
       redirect_to prototype_path
     else
       render :edit
@@ -41,7 +38,7 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
-    prototype = Prototype.find(params[:id])
+    @prototype = Prototype.find(params[:id])
     prototype.destroy
     redirect_to root_path
   end
@@ -51,14 +48,12 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
-  
   def set_prototype
     @prototype = Prototype.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @prototype.user
   end
 
 end
